31aba47bd48d3a4b8238263817476b703b24418a
Aniket762 <aniketindian8@gmail.com>
2021-08-13 17:36:01 +0530

    Feat: Bottom bubble bar added

---
+++ lib/home.dart
@@ -0,0 +1,96 @@
+import 'package:bubble_bottom_bar/bubble_bottom_bar.dart';
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  int currentIndex = 0;
+
+  void changePage(int index) {
+    setState(() {
+      currentIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: <Widget>[
+        Container(
+          color: Colors.redAccent,
+        ),
+        Container(
+          color: Colors.purpleAccent,
+        ),
+        Container(
+          color: Colors.blue,
+        ),
+        Container(
+          color: Colors.black38,
+        ),
+      ][currentIndex],
+      bottomNavigationBar: BubbleBottomBar(
+        opacity: 0.1,
+        currentIndex: currentIndex,
+        onTap: changePage,
+        borderRadius: BorderRadius.vertical(top: Radius.circular(16)),
+        elevation: 8,
+        items: <BubbleBottomBarItem>[
+          BubbleBottomBarItem(
+              backgroundColor: Colors.red,
+              icon: Icon(
+                Icons.dashboard,
+                color: Colors.black,
+              ),
+              activeIcon: Icon(
+                Icons.dashboard,
+                color: Colors.red,
+              ),
+              title: Text("Home")),
+          BubbleBottomBarItem(
+              backgroundColor: Colors.deepPurple,
+              icon: Icon(
+                Icons.access_time,
+                color: Colors.black,
+              ),
+              activeIcon: Icon(
+                Icons.access_time,
+                color: Colors.deepPurple,
+              ),
+              title: Text("Logs")),
+          BubbleBottomBarItem(
+              backgroundColor: Colors.indigo,
+              icon: Icon(
+                Icons.folder_open,
+                color: Colors.black,
+              ),
+              activeIcon: Icon(
+                Icons.folder_open,
+                color: Colors.indigo,
+              ),
+              title: Text("Folders")),
+          BubbleBottomBarItem(
+              backgroundColor: Colors.black,
+              icon: Container(
+                height: 36,
+                width: 36,
+                decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    image: DecorationImage(
+                        image: AssetImage('assets/aniket-circle.png')),
+                    boxShadow: [
+                      BoxShadow(
+                          color: Color(0x5c00000),
+                          offset: Offset(0, 1),
+                          blurRadius: 5)
+                    ]),
+              ),
+              title: Text("Profile"))
+        ],
+      ),
+    );
+  }
+}
