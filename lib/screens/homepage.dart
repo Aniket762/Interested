47edf3406ddb93cd5c75ab368328acaf94fac82f
Aniket762 <aniketindian8@gmail.com>
2021-08-26 18:55:10 +0530

    Poem:Readme removed

---
+++ lib/screens/homepage.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return NestedScrollView(
+        headerSliverBuilder: (context, value) {
+          return [
+            SliverToBoxAdapter(
+              child: Padding(
+                padding: EdgeInsets.fromLTRB(25, 10, 25, 25),
+                child: Align(
+                  alignment: Alignment.centerLeft,
+                ),
+              ),
+            )
+          ];
+        },
+        body: Container());
+  }
+}
