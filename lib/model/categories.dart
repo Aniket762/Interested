9591d7392f6651df6117c7b8f3e15d4cbfc6137c
Aniket762 <aniketindian8@gmail.com>
2021-08-29 11:43:20 +0530

    Feat: Controllers configured

---
+++ lib/model/categories.dart
@@ -0,0 +1,12 @@
+class CategoryModel {
+  final name, imageUrl;
+
+  CategoryModel({this.name, this.imageUrl});
+}
+
+List<CategoryModel> categories = [
+  CategoryModel(name: "Category1", imageUrl: ""),
+  CategoryModel(name: "Category2", imageUrl: ""),
+  CategoryModel(name: "Category3", imageUrl: ""),
+  CategoryModel(name: "Category4", imageUrl: ""),
+];
--- lib/screens/homepage.dart
+++ lib/screens/homepage.dart
@@ -1,14 +1,47 @@
 import 'package:flutter/material.dart';
+import 'package:interested/model/categories.dart';
 
 class HomePage extends StatefulWidget {
   @override
   _HomePageState createState() => _HomePageState();
 }
 
-class _HomePageState extends State<HomePage> {
+class _HomePageState extends State<HomePage>
+    with SingleTickerProviderStateMixin {
+  late ScrollController _scrollController;
+  late TabController _tabController;
+  int currentIndex = 0;
+
+  _smoothScrollToTop() {
+    _scrollController.animateTo(0,
+        duration: Duration(microseconds: 300), curve: Curves.ease);
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _scrollController = ScrollController();
+    _tabController = TabController(length: categories.length, vsync: this);
+    _tabController.addListener(_smoothScrollToTop);
+  }
+
+  changePage(int index) {
+    setState(() {
+      currentIndex = index;
+    });
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _scrollController.dispose();
+    _tabController.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return NestedScrollView(
+        controller: _scrollController,
         headerSliverBuilder: (context, value) {
           return [
             SliverToBoxAdapter(
@@ -17,18 +50,43 @@ class _HomePageState extends State<HomePage> {
                 child: Align(
                   alignment: Alignment.centerLeft,
                   child: Text(
-                    "Heading Top News",
+                    "Kuch badhiya heading",
                     style: TextStyle(
-                        fontFamily: "Times",
+                        // fontFamily: "Times",
                         fontSize: 30,
                         fontWeight: FontWeight.w700),
                     textAlign: TextAlign.left,
                   ),
                 ),
               ),
-            )
+            ),
+            SliverToBoxAdapter(
+              child: Container(
+                padding: EdgeInsets.only(left: 15),
+                alignment: Alignment.centerLeft,
+                child: TabBar(
+                    labelPadding: EdgeInsets.only(right: 15),
+                    indicatorSize: TabBarIndicatorSize.label,
+                    controller: _tabController,
+                    isScrollable: true,
+                    indicator: UnderlineTabIndicator(),
+                    labelColor: Colors.black,
+                    labelStyle:
+                        TextStyle(fontSize: 19, fontWeight: FontWeight.bold),
+                    unselectedLabelColor: Colors.black45,
+                    unselectedLabelStyle:
+                        TextStyle(fontSize: 18, fontWeight: FontWeight.normal),
+                    tabs: List.generate(categories.length,
+                        (index) => Text(categories[index].name))),
+              ),
+            ),
           ];
         },
-        body: Container());
+        body: Container(
+          child: TabBarView(
+            controller: _tabController,
+            children: List.generate(categories.length, (index) => Container()),
+          ),
+        ));
   }
 }
